6b1ce4ceea5c41f5fbd997e3c6840b5524239b49
Shivani <bargeshivani93@gmail.com>
2025-02-16 10:43:09 +0530

    updated DockerFile and created docker-compose.yml

--- app_modified.py
+++ app_modified.py
@@ -182,5 +182,5 @@ def static_files(filename):
     return send_from_directory(OUTPUT_DIR, filename)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
