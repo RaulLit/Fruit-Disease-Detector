69218e57d91258edc57b326367c10dc5ea4025d5
Shivani <bargeshivani93@gmail.com>
2025-02-18 16:44:54 +0530

    Added a comment to log running on which port

--- app_modified.py
+++ app_modified.py
@@ -184,3 +184,4 @@ def static_files(filename):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
+    print(f"Applicationo running on port {port}")
